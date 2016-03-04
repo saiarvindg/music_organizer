5dd5eacf33b4cf784ec50e4a64a03baaadb1aac1
Ramsey Khadder <ramseykhadder@hotmail.com>
2016-03-03 16:35:56 -0800

    RK - initial commit

---
+++ directory.py
@@ -0,0 +1,8 @@
+from metadata import clear_meta, change_meta
+
+def collapse():
+	clear_meta("file")
+	change_meta("file")
+	pass
+
+collapse()
---
+++ metadata/__init__.py
@@ -0,0 +1,7 @@
+def clear_meta(file):
+	print("clear_meta > " + file)
+	pass
+
+def change_meta(file):
+	print("change_meta > " + file)
+	pass
