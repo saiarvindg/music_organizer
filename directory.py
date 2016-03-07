e5e98c80689de63ab8d4d80c8eba60c667cec51c
SolomonKritz <solomonkritz@gmail.com>
2016-03-07 17:11:27 -0500

    SK + SG
    Added basic functionality (hopefully). Should recursively delete
    directories and move files upwards.

--- directory.py
+++ directory.py
@@ -1,8 +1,13 @@
+import os
 from metadata import clear_meta, change_meta
 
 def collapse():
-	clear_meta("file")
-	change_meta("file")
-	pass
+   goDown(path, path)
 
-collapse()
+def goDown(path, oldPath):
+   for file in os.listdir(path):
+      if os.path.isdir(file):
+         goDown(path+file, path)
+   for file in os.listdir(path):
+      os.rename(path+ffile, oldPath+file)
+        
