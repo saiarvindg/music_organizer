05f4233e1420b9e2f61a870e3f281c5c92451269
SolomonKritz <solomonkritz@gmail.com>
2016-03-07 17:56:51 -0500

    SK+SG: Uploading music exts file

--- directory.py
+++ directory.py
@@ -5,24 +5,23 @@ path = '/Users/saiarvind/Downloads/testMusicFolder/Music/'
 
 
 def collapse():
-   print("called collapse!\n")
    goDown(path, path)
+   removeNonAudio(path)
 
 def goDown(path, oldPath):
    for file in os.listdir(path):
       file+="/"
       print("Path " + path +" Old path: " +oldPath + "\n")
       if os.path.isdir(path+file):
-         print("going down")
          goDown(path+file, path)
-   
    for file in os.listdir(path):
       if(os.path.isdir(path+file)):
          os.rmdir(path+file)
       elif(path != oldPath):
          os.rename(path+file, oldPath+file)
    
-
+def removeNonAudio():
+   
+   
 if __name__ == "__main__":
-    print("main\n")
     collapse()
