3e6f9f6d5c9902e10e67f0a4c82403d9e208f9d4
Sai Arvind <sai.arvindg@gmail.com>
2016-03-07 17:29:25 -0500

    SK + SG - so Solomon can see it

--- directory.py
+++ directory.py
@@ -1,13 +1,32 @@
 import os
 from metadata import clear_meta, change_meta
 
+path = '/Users/saiarvind/Downloads/testMusicFolder/Music/'
+
+
 def collapse():
+   print("called collapse!\n")
    goDown(path, path)
 
 def goDown(path, oldPath):
    for file in os.listdir(path):
-      if os.path.isdir(file):
+      file+="/"
+      print("Path " + path +" Old path: " +oldPath + "\n")
+      if os.path.isdir(path+file):
+         print("going down")
          goDown(path+file, path)
-   for file in os.listdir(path):
-      os.rename(path+ffile, oldPath+file)
+      if(path != oldPath):
+       	 for file in os.listdir(path+file):
+            os.rename(path+file, oldPath+file)
+   
         
+
+        
+#def __main__():
+#    print("called main")
+#    collapse()
+
+
+if __name__ == "__main__":
+    print("main\n")
+    collapse()
