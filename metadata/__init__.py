a20a4802f4136c35cb6b5fde55d9afa7ec934860
yyy4401 <aleksey.bychkov@gmail.com>
2016-03-29 19:16:54 -0400

    Change MP3
    Wrote a clear MP3 and a change MP3 methods using the eyed3 python
    library

--- metadata/__init__.py
+++ metadata/__init__.py
@@ -1,7 +1,29 @@
-def clear_meta(file):
+import eyed3
+
+def clear_meta_MP3(file):
+	audiofile = eyed3.load(file)
+	audiofile.tag.artist = u""
+	audiofile.tag.album = u""
+	audiofile.tag.album_artist = u""
+	audiofile.tag.title = u""
+	audiofile.tag.track_num = 0
+
+	audiofile.tag.save()
 	print("clear_meta > " + file)
 	pass
 
-def change_meta(file):
+def clear_meta_MP3(file, artist, album, album_artist, title, track_num):
+	audiofile = eyed3.load(file)
+	audiofile.tag.artist = artist
+	audiofile.tag.album = album
+	audiofile.tag.album_artist = album_artist
+	audiofile.tag.title = title
+	audiofile.tag.track_num = track_num
+
+	audiofile.tag.save()
+	print("Updated MP3 Tags > " + file)
+	pass	
+	
+def change_meta_MP3(file):
 	print("change_meta > " + file)
 	pass
